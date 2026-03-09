4f3df2f794ae7c60123e40e5e230edafea96c21c
Uday2208 <wahyuheryanto22@gmail.com.com>
2026-03-09 07:15:07 +0700

    Fix file inclusion paths in admin directory for Vercel deployment

--- admin/header.php
+++ admin/header.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once "../config/database.php";
-require_once "../config/helpers.php";
+require_once __DIR__ . "/../config/database.php";
+require_once __DIR__ . "/../config/helpers.php";
 
 if (!isset($_SESSION['admin'])) {
     header("Location: login.php");
